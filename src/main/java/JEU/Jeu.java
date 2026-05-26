6ffd50a6a58b28b32a2b5daf9834f9b27e75d660
Clauriane <cloclo24092005@gmail.com>
2026-05-26 22:31:43 +0200

    debut de l'arborescence et classe ouvrant la fenetr graphique

---
+++ src/main/java/JEU/Jeu.java
@@ -0,0 +1,17 @@
+package JEU;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Jeu extends Application{
+
+    @Override
+    public void start(Stage stage){
+        stage.setTitle("LE SOLITAIRE");
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
